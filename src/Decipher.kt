99437acbb8a74c4d85a6ef35a45a8a116dc8091c
pbount <pavlos.bountagkidis@outlook.com>
2018-03-22 15:13:28 +0000

    Added joint solution and description of coursework for preperation of submission.

--- src/Decipher.kt
+++ src/Decipher.kt
@@ -1,96 +1,33 @@
-fun main(args: Array<String>){
-    val stringForSecondSetOfRules = encipher("Good morning Alabama world",1)
-    val t:String = "A wiki is run using wiki software, otherwise known as a wiki engine. A wiki engine is a type of content management system, but it differs from most other such systems"
-    val testingNewDecipherMethod = "No. If the pairing is braking down you should inform the instructor as soon as possible. If this is too near the submission date, and you completed the remainder of the assignment on your own, then you should turn in your assignment with just your name on it. You should state who you worked with and the reason why you did not finish the assignment together."
-    val e:String = encipher(testingNewDecipherMethod ,16)
-    val com:CharArray = charArrayOf('e', 't', 'a', 'o', 'i', 'n', 's', 'r', 'h')
-    val result = decipher(stringForSecondSetOfRules)
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
-    println(result)
+//--------------- "Helper" Functions ---------------//
 
+fun shiftString(str: String, n: Int):String {
+
+    val s: Int = 26 - modulus(n,26)
+    return encipher(str,s)
 }
 
-fun secondStep(set: Set<Int>, string: String): Set<Int> {
-    return set.filter {
-
-        setOfRules(shiftString(string,it))
-    }.toSet()
+fun getLargest(i: Map<Char, Int>, n:Int):Map<Char, Int>{
+    return i.toList().sortedBy { (_, value) -> value.inv()}.take(n).toMap()
 }
 
-/**
- *    Receives a string and returns true if it passes a set of defined rules.
- */
-fun setOfRules(string: String): Boolean {
+fun modulus(i: Int, j: Int):Int {
 
-    return doesntEndWith(string) && singleLetterWorlds(string) &&
-            if(string.contains("q",true)) { afterQisU(string) } else {
-                true
-            } && !string.contains("sx") && doesntContainOnlyConsonants(string)
-
+    return (i+j)%j
 }
 
-fun decipher(string: String): String {
-    // function for this
-    val charCount: Map<Char, Int> = string.toLowerCase().replace(" ", "").groupingBy { it }.eachCount()
-    val mostChars: Map<Char, Int> = getLargest(charCount, 5)
-    val largestChar: Char = getLargest(charCount, 1).keys.first()
-    val mostFrequentCharactersArray = charArrayOf('e', 't', 'a', 'o', 'i', 'n', 's', 'r', 'h')
-    val setOfKeys = mutableSetOf<Int>()
-
-    for (i in mostFrequentCharactersArray) {
-        // get step from highest occurring character from first expected character
-        val step = largestChar - i
-
-        // get array based on that offset (need a charArray)
-        val exp: CharArray = offset(mostChars.keys.toCharArray(), step)
-
-        // find if the char Array exists in the most frequent characters array
-        val comp: Boolean = mostFrequentCharactersArray.compare(exp)
-
-        if (comp) {
-            // array keys
-            // check array.size > 1
-            // array [1,6,9] -> result
-            // dictionary
-            setOfKeys.add(step)
-            println("Adding step to list:  $step")
-            //return shiftString(string,step)
-        }
-    }
-
-    when (setOfKeys.size) {
-        1 -> return shiftString(string, setOfKeys.first())
-        0 -> {
-            //no keys where returned from step 1
-            println("second step with zero keys from step 1")
-            val keys = secondStep((1..25).toSet(), string)
-            return when (keys.size) {
-                1 -> shiftString(string, keys.first())
-                else -> {
-                    println(keys.size)
-                    "dictionary"
-                }
-            //dictionary
-            }
-        }
-        else -> {
-            //keys returned from step 1 are more than 1
-            val fewerKeys = secondStep(setOfKeys, string)
-            println("second step with some keys from step 1")
-            return when (fewerKeys.size) {
-                1 -> shiftString(string, fewerKeys.first())
-                else -> "dictionary"
-            }
-        }
-    }
+fun offset(input:CharArray, value:Int):CharArray {
+    return shiftString(input.joinToString().replace(", ",""), value).toCharArray()
 }
 
-/**
- *    Rule: A word should not end with I, U, V, J
- *    Receives a multi-word sentence as a single string and returns true if each word
- *    in the sentence does not end with I,U,V,J.
- *    Example: [This is a test] -> true
- */
+fun splitString(string: String): List<String> = string.split(" ")
+
+fun wasItSuccessful(set: Set<Int>) = set.size == 1
+
+//---------- Rules for evaluating strings ----------//
+
 fun doesntEndWith(string: String): Boolean {
 
     val splitString = splitString(string.toLowerCase())
@@ -105,12 +42,6 @@ fun doesntEndWith(string: String): Boolean {
     }
 }
 
-/**
- *    Rule: Single letter words can only be I or A.
- *    Receives a a multi-word sentence as a single string and returns true if each word
- *    in the sentence is either I or A.
- *    Example: [This is a test] -> true
- */
 fun singleLetterWorlds(string: String): Boolean {
     val splitString = splitString(string.toLowerCase())
 
@@ -120,78 +51,180 @@ fun singleLetterWorlds(string: String): Boolean {
     }
 }
 
-/**
- *    Rule: Every appearance of the character Q should be followed by U unless Q is the
- *    last character of the word.
- *    Receives a a multi-word sentence as a single string and returns true if each word
- *    in the sentence which contains the character Q has U immidtatelly after it unless
- *    Q is the final character of the word.
- *    Example: [This is a question] -> true
- */
 fun afterQisU(string: String): Boolean {
 
     val splitString = splitString(string.toLowerCase())
 
     return splitString.all { !it.dropLast(1).replace("qu","").contains("q") }
+
+
 }
 
-/**
- *    Rule: A word can not be composed of consonants only.
- *    Receives a a multi-word sentence as a single string and returns true if the word
- *    contains at least one vowel.
- *    Example: [This is a question] -> true
- */
 fun doesntContainOnlyConsonants(string: String): Boolean {
 
     val myCons = listOf('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','x','z','w')
 
-    val splitString = splitString(string.toLowerCase())
+    val splitString = splitString(string.trim().toLowerCase())
 
     return splitString.all { !myCons.containsAll(it.toList()) }
+
 }
 
-/**
- *    Receives a String and an Int and shifts each alphabetical character regardless of letter case
- *    backwards by n characters. All remaining numeric and punctuation characters remain unaffected.
- *    The string received should be a product of the encipher function and the number of steps
- *    should be known.
- *    Example:  [bcde!!] -> [abcd!!]
- */
-fun shiftString(str: String, n: Int):String{
-    val s: Int = 26 - modulus(n,26)
-    return encipher(str,s)
-}
+//------------ CharArray Extension Functions ------------//
 
-/**
- *    Receives a Map<Char, Int> and an Int n and returns the n entries of the Map which
- *    have the highest Int.
- */
-fun getLargest(i: Map<Char, Int>, n:Int):Map<Char, Int>{
-    return i.toList().sortedBy { (_, value) -> value.inv()}.take(n).toMap()
-}
-
-/**
- *    Receives a number i and a number j and returns the result of
- *    i mod j.
- */
-fun modulus(i: Int, j: Int):Int{
-    return (i+j)%j
-}
-
-fun offset(input:CharArray, value:Int):CharArray{
-    return shiftString(input.joinToString().replace(", ",""), value).toCharArray()
-}
+fun String.charCount() = this.toLowerCase().replace(" ", "").groupBy {it}.mapValues { it.value.size }
 
 
-fun CharArray.compare(charToCompareWith: CharArray): Boolean {
-    for(c in charToCompareWith) {
+fun CharArray.containsAll(charArrayToCompare: CharArray): Boolean {
+    for(c in charArrayToCompare) {
         if(!this.contains(c))
             return false
     }
     return true
 }
 
-fun splitString(string: String): List<String> = string.split(" ")
+//------------------ Steps ------------------//
+
+fun firstStep(string: String) : Set<Int> {
+    val charCount = string.charCount()
+    val mostChars = getLargest(charCount, 5)
+    val largestChar = getLargest(charCount, 1).keys.first()
+    val mostFrequentCharacters = charArrayOf('e','t','a','o','i','n','s','r','h')
+    val resultKeys = mutableSetOf<Int>()
+
+    for(i in mostFrequentCharacters) {
+        val step = largestChar - i
+
+        val exp = offset(mostChars.keys.toCharArray(), step)
+
+        val comp = mostFrequentCharacters.containsAll(exp)
+        if(comp) {
+            resultKeys.add(step)
+        }
+    }
+
+    return resultKeys
+}
+
+/**
+ *    Receives a String of a single word and returns true if
+ *    it contains any of the most common double letter occurances.
+ */
+val mostFrequentDoubleLetters = { string: String ->
+
+    tailrec fun loop(string:String): Boolean {
+        return when {
+            string.length <= 1 -> false
+            string[0] == string[1] -> listOf("ss","ee","tt","ff","ll","mm","oo").contains(string.substring(0,2))
+            else -> loop(string.drop(1))
+        }
+    }
+    loop(string)
+}
+
+/**
+ *
+ */
+val heuristicsOfRules = { string: String -> doesntEndWith(string) && singleLetterWorlds(string) &&
+        if(string.contains("q",true)) { afterQisU(string) } else {
+            true
+        } && !string.contains("sx") && doesntContainOnlyConsonants(string) }
 
 
 
+
+val mostFrequentWords = {string: String ->
+
+    //val ROOT = System.getProperty("user.dir") + "/CW2TestingDav"
+    // val beanFactory = ClassPathXmlApplicationContext("file:$ROOT/beans.xml")
+    //Loading a list of string from an xml file with spring
+    val listWithMostFrequentWords = ListWithMostFrequentWords.myList()
+    if(splitString(string).all{it.length > 4})  {
+        true
+    } else {
+        val filteredList = splitString(string).filter {listWithMostFrequentWords.contains(it.toLowerCase())}
+        !filteredList.isEmpty()
+    }
+
+}
+
+object ListWithMostFrequentWords {
+    private val ROOT = System.getProperty("user.dir")
+
+    private val beanFactory: BeanFactory
+        @Throws (Exception::class)
+        get() = ClassPathXmlApplicationContext("file:$ROOT/beans.xml")
+
+    fun myList():List<String> = beanFactory.getBean("myList") as List<String>
+
+}
+
+fun filter(set: Set<Int>, string: String, f: (String) -> Boolean): Set<Int> {
+
+    return  when(set.isEmpty()) {
+        true -> {
+            (1..25).toSet()
+        }
+        else -> set
+    }.filter {
+        f(shiftString(string,it))
+    }.toSet()
+
+}
+
+fun decipher(string: String): String {
+
+    //First Attempt to decipher
+    val setOfKeysAfterFirstStep = firstStep(string)
+    if(wasItSuccessful(setOfKeysAfterFirstStep)) {
+        println("Solved in first attempt")
+        return shiftString(string, setOfKeysAfterFirstStep.first())
+    }
+
+
+    //Second Attempt
+    val setOfKeysAfterSecondStep = filter(setOfKeysAfterFirstStep, string, mostFrequentWords)
+    if(wasItSuccessful(setOfKeysAfterSecondStep)) {
+        println("Solved in second attempt")
+        return shiftString(string, setOfKeysAfterSecondStep.first())
+    }
+
+
+    //Third Attempt
+    val setOfKeysAfterThirdStep = filter(setOfKeysAfterSecondStep,string,mostFrequentDoubleLetters)
+    if(wasItSuccessful(setOfKeysAfterThirdStep)) {
+        println("Solved in third attempt")
+        return shiftString(string, setOfKeysAfterThirdStep.first())
+    }
+
+    //4th Attempt
+    val setOfKeysAfterForthStep = filter(setOfKeysAfterThirdStep,string, heuristicsOfRules)
+    if(wasItSuccessful(setOfKeysAfterForthStep)) {
+        println("Solve after forth attempt")
+        return shiftString(string,setOfKeysAfterForthStep.first())
+    }
+
+
+    return "Dictionary under construction"
+
+}
+
+
+fun main(args: Array<String>){
+
+    val stringForSecondSetOfRules = encipher("It's a beautiful day.!!",1)
+    val t:String = "Good Morning"
+    val failingString = encipher(t,1)
+    val testingNewDecipherMethod = "No. If the pairing is braking down you should inform the instructor as soon as possible. If this is too near the submission date, and you completed the remainder of the assignment on your own, then you should turn in your assignment with just your name on it. You should state who you worked with and the reason why you did not finish the assignment together."
+    val e:String = encipher(testingNewDecipherMethod ,16)
+    val com:CharArray = charArrayOf('e', 't', 'a', 'o', 'i', 'n', 's', 'r', 'h')
+    val result = decipher(failingString)
+
+    println(result)
+    println(System.getProperty("user.dir"))
+
+    println("--------".repeat(4))
+
+    println("Testing Function")
+
+}
--- tests/DecipherTest.kt
+++ tests/DecipherTest.kt
@@ -20,6 +20,19 @@ class DecipherTest {
     }
 
     @Test
+    fun getLargestTest(){
+        val input: Map<Char, Int> = mapOf('a' to 5, 'b' to 4, 'c' to 3, 'd' to 2, 'e' to 6)
+        val expected: Map<Char, Int> = mapOf('e' to 6, 'a' to 5, 'b' to 4)
+        assertEquals(expected, getLargest(input,3))
+
+        val input2: Map<Char, Int> = mapOf('a' to 5)
+        val expected2: Map<Char, Int> = mapOf('a' to 5)
+        assertEquals(expected2, getLargest(input2,3))
+
+
+    }
+
+    @Test
     fun doesntEndWith() {
         val shouldBeTrue: List<String> = listOf(
                 "I am a man who walks alone",
@@ -71,8 +84,8 @@ class DecipherTest {
                 "qr quest Q test"
         )
 
-        shouldPass.forEach { println("Testing: $it"); assertEquals(true, singleLetterWorlds(it)) }
-        shouldFail.forEach { println("Testing: $it"); assertEquals(false, singleLetterWorlds(it)) }
+        shouldPass.forEach { println("Testing: $it"); assertEquals(true, afterQisU(it)) }
+        shouldFail.forEach { println("Testing: $it"); assertEquals(false, afterQisU(it)) }
     }
 
 
@@ -88,23 +101,148 @@ class DecipherTest {
                 "Thls shqvld nGvGr pqss"
         )
 
+        shouldPass.forEach { println("Testing: $it"); assertEquals(true, doesntContainOnlyConsonants(it)) }
+        shouldFail.forEach { println("Testing: $it"); assertEquals(false, doesntContainOnlyConsonants(it)) }
+    }
+
+    @Test
+    fun mostFrequentDoubleLettersTest(){
+        val shouldBeTrue:List<String> = listOf(
+                "Hello world!",
+                "Seeing is believing",
+                "A loop of wires",
+                "See the blizzard",
+                "tt rr gg rr tt ww dd ee"
+        )
+
+        val shouldBeFalse:List<String> = listOf(
+                "bb mm nn bb xx zz",
+                "Just a blizzard",
+                "pizza is love, pizza is life!",
+                "reign of terror",
+                "No doubles in this sentence"
+        )
+
+        shouldBeTrue.forEach { println("Testing: $it"); assertEquals(true, mostFrequentDoubleLetters(it)) }
+        shouldBeFalse.forEach { println("Testing: $it"); assertEquals(false, mostFrequentDoubleLetters(it)) }
+
+    }
+
+    @Test
+    fun mostFrequentWordsTest(){
+        val shouldBeTrue:List<String> = listOf(
+                "This is true because it contains this",
+                "The is also a valid ward",
+                "kl ih gg sd tr to",
+                "!ThIs ?? TO ''iT",
+                "omits larger dictionary entries"
+        )
+
+        val shouldBeFalse:List<String> = listOf(
+                "Non existant dictionary words",
+                "",
+                "N0t 4 W0rd"
+        )
+
+        shouldBeTrue.forEach { println("Testing: $it"); assertEquals(true, mostFrequentWords(it)) }
+        shouldBeFalse.forEach { println("Testing: $it"); assertEquals(false, mostFrequentWords(it)) }
+
+    }
+
+    @Test
+    fun filterTest(){
+        val steps: Set<Int> = (0..25).toSet()
+
+        val accepted:List<String> = listOf(
+                "Look", "book","?see?","gLee!!", "glee", "gull", "riff", "really"
+        )
+
+        val notAccepted:List<String> = listOf(
+                "Todd", "Pizza", "blizzard", "xXx", "B00k is not a word"
+        )
+
+        accepted.forEach {
+            println("Testing: $it")
+            assertEquals(
+                    7,
+                    filter(steps, accepted[accepted.indexOf(it)], mostFrequentDoubleLetters).size)
+        }
+
+        println(filter((0..25).toSet(), "Todd", mostFrequentDoubleLetters))
+
+
+        notAccepted.forEach {
+            println("Testing: $it");
+            assertEquals(0, filter(steps, notAccepted[notAccepted.indexOf(it)], mostFrequentDoubleLetters).size)
+        }
+
+    }
+
+
+    @Test
+    fun setOfRulesTest() {
+        val shouldPass: List<String> = listOf(
+                "Awesome beautiful weather Birmingham not Quest query whatever I am a man who walks alone",
+                "QUrtxfe mjklbnedfg Questing in wow A train",
+                "Even though no single word is in this sentense it still should pass"
+        )
+
+        val shouldFail: List<String> = listOf(
+                "Awesome day t oday",
+                "Stars can t shine without darkness",
+                "C R T",
+                "7 is just a number"
+        )
+
         shouldPass.forEach { println("Testing: $it"); assertEquals(true, singleLetterWorlds(it)) }
         shouldFail.forEach { println("Testing: $it"); assertEquals(false, singleLetterWorlds(it)) }
     }
 
 
     @Test
-    fun setOfRulesTest() {
-        val shouldPass:List<String> = listOf(
-                "Awesome beautiful weather Birmingham not Quest query whatever I am a man who walks alone",
-                "QUrtxfe mjklbnedfg Questing in wow A train"
+    fun wasItSuccessfulTest() {
+        val shouldPass:List<Set<Int>> = listOf(
+                setOf(1), setOf(5), setOf(20)
         )
 
-        val shouldFail:List<String> = listOf(
-                "awesome day sx"
+        val shouldFail:List<Set<Int>> = listOf(
+                setOf<Int>(), setOf(1,2,3), setOf(2,3)
         )
 
-        shouldPass.forEach { println("Testing: $it"); assertEquals(true, singleLetterWorlds(it)) }
-        shouldFail.forEach { println("Testing: $it"); assertEquals(false, singleLetterWorlds(it)) }
+        shouldPass.forEach { assertEquals(true, wasItSuccessful(it)) }
+        shouldFail.forEach { assertEquals(false, wasItSuccessful(it)) }
+    }
+
+    @Test
+    fun containsMostCommonDoubleLettersTest(){
+
+        val listOfStrings = listOf(
+                "Good morning",
+                "Should pass",
+                "I weep everyday for my lost dog",
+                "Player 3 counterAttacks player 2",
+                "The entire staff has done a great job this year.",
+                "He called the police because he was really worried.",
+                "She commanded that work on the bridge cease immediately.",
+                "Fish is a healthy food"
+        )
+
+
+    }
+
+    @Test
+    fun decipherTest() {
+        val shouldPass = "It's a beautiful day.!!"
+        val shouldPass1 = "A wiki is run using wiki software, otherwise known as a wiki engine." +
+                " A wiki engine is a type of content management system, but it differs from most other such "
+        val shouldPass2 = "No. If the pairing is braking down you should inform the instructor as soon as possible."
+        val shouldFail = "Good morning."
+
+        for(i in 1..25) {
+            kotlin.test.assertEquals(shouldPass, decipher(encipher(shouldPass, i)))
+            kotlin.test.assertEquals(shouldPass1, decipher(encipher(shouldPass1, i)))
+            kotlin.test.assertEquals(shouldPass2, decipher(encipher(shouldPass2, i)))
+            kotlin.test.assertEquals(shouldFail, decipher(encipher(shouldFail, i)))
+        }
     }
 }
